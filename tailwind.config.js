51226ab5e82cad5be0ebf3a345007f7ef4f10a9a
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-03-04 21:50:26 +0100

    chore: setup tailwind and project assets

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'main': '#0B0D17',
+      'primary': '#FFFFFF',
+      'secondary': '#D0D6F9',
+    },
+    fontFamily: {
+      sans: ['Barlow Condensed', 'sans-serif'],
+      serif: ['Bellefair', 'serif'],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
