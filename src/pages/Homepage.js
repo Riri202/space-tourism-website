653f26963d3ca258b23b6db6697a1708876ee379
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-03-14 09:15:34 +0100

    ft-display-crew-page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Homepage() {
   return (
-    <div className='flex justify-between p-20 pt-40 w-11/12' >
-      <div className='flex flex-col space-y-4'>
+    <div className='flex justify-between p-20 pt-40 h-screen w-11/12' >
+      <div className='flex flex-col space-y-4 w-1/2'>
         <p className='font-sans text-secondary text-2xl tracking-wider'>SO YOU WANT TO TRAVEL TO</p>
         <h1 className='font-serif text-9xl tracking-widest'>SPACE</h1>
         <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
