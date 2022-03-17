996524578c273df10dfdf42e1ef9f1b621a0a11a
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-03-17 15:24:16 +0100

    style: fix styling

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -41,7 +41,7 @@ function Destination() {
   return (
     <>
       <div className='flex justify-center items-center md:justify-start'>
-      <div className='text-2xl flex pt-10 md:ml-20'><span className='font-thin text-secondary mr-3'>01</span>PICK YOUR DESTINATION</div>
+      <div className='text-2xl flex pt-10 md:ml-20 tracking-widest'><span className='font-bold text-grey mr-3'>01</span>PICK YOUR DESTINATION</div>
       </div>
 
 
@@ -54,44 +54,44 @@ function Destination() {
           return (<>
 
 
-            <div key={key} className='flex flex-col items-center text-center md:text-left md:flex-row md:justify-between p-20'>
+            <div key={key} className='flex flex-col items-center text-center md:text-left md:flex-row md:justify-center md:space-x-40 p-20'>
               <div>
                 <img src={images[place.name]} alt='destination' />
               </div>
 
-              <div className='flex flex-col mt-11 md:mt-0 md:w-1/2 space-y-10'>
+              <div className='flex flex-col mt-11 md:mt-0 md:w-1/3 space-y-10'>
                 <div className='flex justify-center md:justify-start'>    
-                <nav className='flex space-x-6 md:pr-12'>
-                  <button onClick={handleMoon} className='py-3 transition-all ease-out hover:border-b-4 font-sans '>
-                    <p className=' tracking-widest font-thin'>MOON</p>
+                <nav className='flex space-x-6 md:pr-12 text-secondary '>
+                  <button onClick={handleMoon} className='py-3 transition-all ease-out hover:border-b-4 hover:border-primary font-sans '>
+                    <p className='md:text-lg tracking-widest font-thin'>MOON</p>
                   </button>
-                  <button onClick={handleMars} className='py-3 transition-all ease-out hover:border-b-4 font-sans '>
-                    <p className=' tracking-widest font-thin'>MARS</p>
+                  <button onClick={handleMars} className='py-3 transition-all ease-out hover:border-b-4 hover:border-primary font-sans '>
+                    <p className='md:text-lg tracking-widest font-thin'>MARS</p>
                   </button>
-                  <button onClick={handleEuropa} className='py-3 transition-all ease-out hover:border-b-4 font-sans '>
+                  <button onClick={handleEuropa} className='py-3 transition-all ease-out hover:border-b-4 hover:border-primary font-sans '>
                     <p className=' tracking-widest font-thin'>EUROPA</p>
                   </button>
-                  <button onClick={handleTitan} className='py-3 transition-all ease-out hover:border-b-4 font-sans '>
-                    <p className=' tracking-widest font-thin'>TITAN</p>
+                  <button onClick={handleTitan} className='py-3 transition-all ease-out hover:border-b-4 hover:border-primary font-sans '>
+                    <p className='md:text-lg tracking-widest font-thin'>TITAN</p>
                   </button>
                 </nav>
                 </div>
 
-                <h1 className='text-6xl tracking-wider font-serif'>{place.name === destination ? place.name : null}</h1>
+                <h1 className='text-6xl tracking-wider uppercase font-serif'>{place.name === destination ? place.name : null}</h1>
 
                 <div>
-                  <p> {place.description}</p>
+                  <p className='md:text-lg text-secondary'> {place.description}</p>
                 </div>
-                <div className='ring-top ring-1 w-full ring-secondary'></div>
+                <div className='ring-top ring-1 w-full ring-secondary '></div>
                 <div className='flex  flex-col space-y-5 text-center md:flex-row md:text-left md:space-y-0 md:space-x-12'>
-                  <div>
-                    <p>AVG. DISTANCE</p>
-                    <p>{place.distance}</p>
+                  <div className='flex flex-col space-y-3'>
+                    <p className='text-sm font-sans text-secondary tracking-widest'>AVG. DISTANCE</p>
+                    <p className='text-3xl font-serif uppercase tracking-widest'>{place.distance}</p>
                   </div>
 
-                  <div>
-                    <p>EST. TRAVEL TIME</p>
-                    <p>{place.travel}</p>
+                  <div className='flex flex-col space-y-3'>
+                    <p className='text-sm font-sans text-secondary tracking-widest'>EST. TRAVEL TIME</p>
+                    <p className='text-3xl font-serif uppercase tracking-widest'>{place.travel}</p>
                   </div>
                 </div>
               </div>
